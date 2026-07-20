64dc7553786dac1ff0defaafcee903aec8eb0f79
SkyBird233 <52884766+SkyBird233@users.noreply.github.com>
2026-07-20 17:45:08 +0800

    feat: generate frontend client using openapi-ts

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,6 +6,7 @@ from app.api.router import router
 app = FastAPI(
     title="QAshboard",
     openapi_url=f"{settings.API_V1}/openapi.json",
+    generate_unique_id_function=lambda route: route.name,
 )
 
 app.include_router(router, prefix=settings.API_V1)
