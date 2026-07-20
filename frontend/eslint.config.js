f7d684fc54a4d24cbfbc03ce91b7876b6eeb3b9d
SkyBird233 <52884766+SkyBird233@users.noreply.github.com>
2026-07-20 16:16:05 +0800

    chore(frontend): setup prettier

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,5 +1,9 @@
+import eslintConfigPrettier from 'eslint-config-prettier/flat'
 import vuetify from 'eslint-config-vuetify'
 
-export default vuetify({
-  ts: true,
-})
+export default [
+  vuetify({
+    ts: true,
+  }),
+  eslintConfigPrettier,
+]
