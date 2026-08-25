b50c0470b883e3a0cb28339aa81ad45629745a2b
SkyBird233 <52884766+SkyBird233@users.noreply.github.com>
2026-08-25 17:46:52 +0800

    feat(backend/cli): update help messages

--- backend/app/cli.py
+++ backend/app/cli.py
@@ -11,20 +11,20 @@ buildbot_cli = Typer(help="Manage contracts with magical buildbots")
 cli.add_typer(buildbot_cli, name="buildbot")
 
 
-@buildbot_cli.command(
-    help="Make a contract with [bold]Q[/bold]Ash[bold]b[/bold]oard and become a magical builtbot."
-)
+@buildbot_cli.command()
 def create(name: str, display_name: str | None = None):
+    """Make a contract with QB and become a magical builtbot."""
     with Session(engine) as session:
         token = create_buildbot(
             session, BuildBotCreate(name=name, display_name=display_name)
         )
         session.commit()
-    print(token)
+    print(name, token)
 
 
 @buildbot_cli.command()
 def set(buildbot_name: str, enabled: bool):
+    """Awaken a magical buildbot or send it into an enchanted slumber."""
     with Session(engine) as session:
         set_buildbot(session, buildbot_name, enabled)
         session.commit()
@@ -32,14 +32,16 @@ def set(buildbot_name: str, enabled: bool):
 
 @buildbot_cli.command()
 def rotate(buildbot_name: str):
+    """Bestow a new secret token upon a magical buildbot."""
     with Session(engine) as session:
         token = rotate_token(session, buildbot_name)
         session.commit()
-    print(token)
+    print(buildbot_name, token)
 
 
 @buildbot_cli.command(name="list")
 def ls():
+    """Reveal every magical buildbot under contract with QB."""
     with Session(engine) as session:
         buildbots = session.exec(select(BuildBot.name).distinct()).all()
         session.commit()
